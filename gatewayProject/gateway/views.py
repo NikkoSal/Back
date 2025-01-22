c1f697410254aa78174fb51bed04c55c19f940d6
Nikita Seliverstov <gamebot202@mail.ru>
2025-01-22 21:12:43 +1000

    update

--- gatewayProject/gateway/views.py
+++ gatewayProject/gateway/views.py
@@ -1,14 +1,15 @@
-import requests
 from django.http import HttpResponse
+import requests
 
-url = "http://127.0.0.1:8000/myapp6/"
+
+with open(".env") as f:
+    url = f.readline().strip()
+
 def proxy_request(request):
-
     response = requests.get(url)
 
     return HttpResponse(
         response.text,
         status=response.status_code,
         content_type=response.headers.get('Content-Type', 'text/plain')
-
     )
